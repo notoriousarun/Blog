8ec98bd0bd782afb1095a3ed1ee8e91a974ecde5
Arun <aruntakkar5@gmail.com>
2019-02-28 23:25:48 +0530

    "static path updated"

--- blog/settings.py
+++ blog/settings.py
@@ -125,6 +125,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = '/'
 
